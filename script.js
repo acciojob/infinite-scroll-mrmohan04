57c2f319164db3703dea47de4b1797543c55e3bf
mrmohan04 <gugulothmohan381@gmail.com>
2025-08-17 23:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-//your code here!
+// script.js
 
+const list = document.getElementById("infi-list");
+let itemCount = 0;
+
+// Function to add list items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    itemCount++;
+    const li = document.createElement("li");
+    li.textContent = "Item " + itemCount;
+    list.appendChild(li);
+  }
+}
+
+
+addItems(10);
+
+const observer = new IntersectionObserver((entries) => {
+  const lastEntry = entries[0];
+  if (lastEntry.isIntersecting) {
+    
+    addItems(2);
+    
+    observer.unobserve(lastEntry.target);
+    observer.observe(list.lastElementChild);
+  }
+}, {
+  root: list,        
+  rootMargin: "0px",
+  threshold: 1.0    
+});
+
+
+observer.observe(list.lastElementChild);
